c7380c0d4cd3e5ab2db460699f472e3329dc7079
Derek Rogers <haethos.dr@gmail.com>
2024-04-03 22:45:27 -0700

    Finished overall calculator. Added del button, refactored some code, fixed bugs and created shrinking font. Still can use some css touch up, micro refactors, and maybe a hidden/non obvious bug to be found. Also need keyboard functionality.

--- calculator.js
+++ calculator.js
@@ -7,134 +7,179 @@
 // notes for 4/1: decimalNumber / 1.0 isnt yeildig a decimal, start there tomorrow to first find a decimal conversion method //
 // issues as of 5pm 4/1: if you hit '=' button multiple times in a row, it glitches and stops working.
 // what can i use arguements parameter object for?
+
+// and refactoring and CSS, need only 1 decimal per operation and limitng digits
+//can convert number to string and slice if over length of 10 and convert back (or array.from --> slice..---> join)
+//to 14. also need to set text on the other side.
+
 const inputField = document.querySelector('#displayField');
 const numberButtons = document.querySelectorAll('.number-button');
 const calculatorButtons = document.querySelectorAll('.button');
+const clearButton = document.querySelector('#clear');
+const opColorButtons = document.querySelectorAll('.operate');
 
 let numberInput;
 let operatorInput;
-let firstNumber;
+let accumulatorNumber;
 let secondNumber;
 let activeOperator;
+let decimalButtonState = false;
 
-function inputFieldText(calculatorField) {
-    inputField.textContent = calculatorField; 
+// need to limit numberfield length to 14 until shrinks //
+//then git add / commit. then, refactor //
+// add backspace functionality, so the user can undo if they click the wrong button
+
+function updateNumberField(calculatorField = 0, fontSize) {
+    inputField.textContent = calculatorField;
+    inputField.style.fontSize = fontSize;
 };
-inputFieldText(0);
+updateNumberField();
+
+// function updateButtonColor(buttonValue) {
+//     opColorButtons.forEach(button => {
+//         if (button.dataset.colored) {
+//             button.dataset.colored = false;
+//             button.style.color = 'black';
+//         }
+//         if (buttonValue === button.textContent) {
+//             button.style.color = 'darkred';
+//             button.dataset.colored = true;
+//         }
+//     })
+// };
+
+
+function updateButtonColor(buttonValue) {
+    opColorButtons.forEach(button => {
+        if (button.style.color === 'darkred') {
+            button.style.color = 'black';
+        }
+        if (buttonValue === button.textContent) {
+            button.style.color = 'darkred';
+        }
+    })
+};
+
+function resizeText() {
+    const inputField = document.querySelector('#displayField');
+    const maxWidth = inputField.offsetWidth;
+    let fontSize = 47;
+    while(inputField.scrollWidth > maxWidth) {
+        fontSize--;
+        inputField.style.fontSize = fontSize + 'px';
+    }
+};
 
 numberButtons.forEach(button => {
     button.addEventListener('click', (event) => {
-        if (numberInput) {numberInput += event.target.textContent;}
-        else {numberInput = event.target.textContent;};
-        inputFieldText(numberInput);
-        // will need something to clear the operator button color;
+        if (numberInput) { numberInput += event.target.textContent; }
+        else { numberInput = event.target.textContent; };
+        updateNumberField(numberInput);
+        resizeText();
+        clearButton.textContent = 'C';
     });
 });
 
 calculatorButtons.forEach(button => {
     button.addEventListener('click', (event) => {
-    let numAffector = event.target.textContent; 
-        calculatorAffector(numAffector);
-    });
+    let numAffector = event.target.textContent;         
+    calculatorAffectors(numAffector);
+    resizeText(); // still finding best place for this. here? //
+    })
 });
 
-function calculatorAffector(numAffector) {
+function calculatorAffectors(numAffector) {
     switch (numAffector) {
+        case "del" :
         case "." :
         case "AC" :
+        case "C" :
         case "%" :
         case "+/-" : 
-            console.log(numAffector);
             operatorLookup[numAffector]();
         break;
         default:
             numberInput = parseFloat(numberInput);
             operatorInput = numAffector;
-            console.log(numAffector);
-            operate(operatorInput);
+            operateLogic(operatorInput);
+        if ((operatorInput || activeOperator) && numAffector !=="=") {updateButtonColor(numAffector)};
     };
 };
- 
-// priority: now %, then AC simplification, then +/-, then operator button color, then refactor, then CSS.
 
-function operate(operatorInput) {
-    if (firstNumber && activeOperator !== null) {  // #2 // 
-        secondNumber = numberInput;
-        firstNumber = operatorLookup[activeOperator](firstNumber, secondNumber);
-        inputFieldText(firstNumber);
+function operateLogic(operatorInput) {
+    if (accumulatorNumber && activeOperator) {
+        secondNumber = numberInput; // do i need this? or insert numberInput where second num is? //
+        accumulatorNumber = operatorLookup[activeOperator](accumulatorNumber, secondNumber);
+        updateNumberField(accumulatorNumber);
         activeOperator = operatorInput;
         if (operatorInput === '=') {[activeOperator, secondNumber] = [null, null]};
-    } else if (firstNumber && activeOperator === null) {  // #4 //
+        updateButtonColor(operatorInput);
+    } else if (accumulatorNumber && activeOperator === null) {
         activeOperator = operatorInput;
-    } else { // #1 // ///look here for %
-        firstNumber = numberInput;
+    } else {
+        accumulatorNumber = numberInput;
         activeOperator = operatorInput;
     };
-    [numberInput, operatorInput] = [null, null]; //if i want to make numberInput = field, will need to change this.
+    [numberInput, operatorInput] = [null, null];
 };
 
-let operatorLookup = {     // @ the end, could maybe reduce this. //
-    '+': (firstNumber, secondNumber) => firstNumber + secondNumber,
-    '-': (firstNumber, secondNumber) => firstNumber - secondNumber,
-    '*': (firstNumber, secondNumber) => firstNumber * secondNumber,
-    '/': (firstNumber, secondNumber) => firstNumber / secondNumber,
-    '%': () => {
-                numberInput = (parseInt(numberInput) * 0.01).toString();
-                inputFieldText(numberInput);
-            },
-    //'=': (firstNumber, secondNumber) => {this.operatorLookup[firstOperator](firstNumber, secondNumber);
-                                       // [firstOperator, secondNumber] = [null, null]}, //fix this
-    '.': () => {if (numberInput === null) { numberInput = '0.' }
-                else { numberInput += '.' };            
-                inputFieldText(numberInput);
-            },    
+let operatorLookup = {
+    '+': (accumulatorNumber, secondNumber) => accumulatorNumber + secondNumber,
+    '-': (accumulatorNumber, secondNumber) => accumulatorNumber - secondNumber,
+    '*': (accumulatorNumber, secondNumber) => accumulatorNumber * secondNumber,
+    '/': (accumulatorNumber, secondNumber) => accumulatorNumber / secondNumber,
+    '%': () => { 
+        if (accumulatorNumber && numberInput === null) {
+            accumulatorNumber *= 0.01;
+            updateNumberField(accumulatorNumber);
+        } else {
+            numberInput = (parseFloat(numberInput) * 0.01).toString();
+            updateNumberField(numberInput);
+        };  // resizeText();
+    },
+    '.': () => {
+        if (numberInput === null) { decimalButtonState = true; numberInput = '0.'; }
+        else if (decimalButtonState === false) { numberInput += '.' };            
+            updateNumberField(numberInput);
+    },    
     'AC': () => {
-                [numberInput, operatorInput, firstNumber, secondNumber, activeOperator] = [null, null, null, null, null];
-                inputFieldText(0);  
-            },
-    'C': ('filler...'),
-    '+/-': ('filler...'), // toggles number between positive or negative. //
+        [numberInput, operatorInput, accumulatorNumber, secondNumber, activeOperator] = [null, null, null, null, null];
+            decimalButtonState = false;
+            updateNumberField(0, '47px');
+            updateButtonColor();
+    },
+    'C': () => {
+        if (numberInput) {
+            numberInput = null;
+            updateNumberField(0, '47px');
+        } else if (accumulatorNumber && activeOperator === null) {
+            accumulatorNumber = null;
+            updateNumberField(0, '47px');
+        } else if (accumulatorNumber && activeOperator) { operatorInput = null; activeOperator = null; }
+            clearButton.textContent = 'AC';
+            updateButtonColor();
+            decimalButtonState = false;
+    },
+    '+/-': () => { 
+        if (accumulatorNumber && numberInput === null) {
+            accumulatorNumber *= -1;
+            return updateNumberField(accumulatorNumber);
+        } else if (numberInput === null) { numberInput = '-' }
+        else {    
+        numberInput = (parseFloat(numberInput) * -1).toString() };
+        updateNumberField(numberInput);
+    },
+    'del': () => {
+        if (numberInput) {
+            console.log("del");
+            numberInput = numberInput.slice(0, -1);
+            updateNumberField(numberInput);
+        };
+    },
 };
 
-////////// past code ///////////
-//if (operatorInput === '.') {operatorLookup[operatorInput]}; // need to immediately put decimal on screen and with numberInput field
-//////////////////////
-// #4 //    // or (secondNumber = null && firstOperator = null)? // what is clearer? //
-//////////////////////
-// console.log(`firstNumber: ${firstNumber}`);
-// console.log(`firstOperator: ${firstOperator}`);
-// console.log(`numberInput: ${numberInput}`);
-// if (event.target.id === 'plus' || 'minus' || 'multiply' || 'divide' || 'equal') { ///come back to this. should be class
-//     operatorInput = event.target.textContent;
-//     operate(operatorInput);
-// };
-///////////////////////
-// const subtract = function(...numbers) {
-//     return numbers.reduce((accumulator, currentNumber) => accumulator - currentNumber);
-// };
-// if (operatorInput === '=') { // #3 //    ///recent change
-//     secondNumber = numberInput;
-//     firstNumber = operatorLookup[firstOperator](firstNumber, secondNumber);
-//     inputField.textContent = firstNumber;
-//     [firstOperator, secondNumber] = [null, null]; //can also add : operatorInput = null, but not necessary//
-// } else 
-////////////////////////
-// if (numAffector === '.') { // this will go to lookup table // ideally clear will also (as in: everything but operators (+,,-,/,*,=)).
-//     console.log(numAffector);
-//     operatorLookup[numAffector](); // when its  just the firstNumber, i cant ad a decimal
-// } else if (event.target.id === 'clear') {
-//     [numberInput, operatorInput, firstNumber, secondNumber, firstOperator] = [null, null, null, null, null];
-//     inputField.textContent = 0;
-// } else {
-//     numberInput = parseFloat(numberInput); //still converts string to number
-//     console.log(numberInput);
-//     //numberInput = parseInt(numberInput); // can also put this in operate();
-//     operatorInput = numAffector;
-//     operate(operatorInput);
-//     console.log(firstNumber);
-// };
 /////////////////////////
-// notes/learn:  need buttons.forEach if there are multiple buttons in one button variable , 
+// notes/learn:  need buttons.forEach if there are multiple buttons in one button variable , (how did i forget?)
 // otherwise it wont work– not specific enough
 // //still converts string to number, with or w/o decimal.
 // operatorLookup[nubby](): need to have brackets if i am doing an object lookup w/ a function, even if im not passing a number to the parameter
@@ -144,13 +189,6 @@ let operatorLookup = {     // @ the end, could maybe reduce this. //
 // here when the next operator is pressed (so far for !'=' operators)
 // #3: equal does the same, but clears the first Operator and secondNumber
 // #4: For doing a new operation after another operation. (e.g. 3+3=6+'...')
-// if an operation has already happend, and the result is in firstNumber.
+// if an operation has already happend, and the result is in accumulatorNumber.
 // it will go to #2 or #3 for next operation
 //////////////////////////
-//1//  press: 3; -> firstNumber = 3; //2//  press +. -> firstOperator = + & operatorInput = + & firstOperator = +.
-//3//  press: 3; -> numberInput = 3, inputField = 3 (via event target). 
-//4//  press: '='; -> secondNumber = numberInput; firstNumber = operatorLookup[firstOperator](firstNumber, secondNumber);
-// inputField = firstNumber; [firstOperator, secondNumber, numberInput] = [null, null, null];
-//5// AS OF NOW: firstNumber = 6 & firstOperator = null & secondNumber = null & numberInput = null &o operatorInput = 'null'
-//6// press '+'; -> 
-
